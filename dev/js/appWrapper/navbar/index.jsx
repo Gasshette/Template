659274d10af8369209d991b078d3e6ba2d1933ea
Quentin GRISEL <quentin.grisel@capgemini.com>
2018-09-17 17:17:23 +0200

    Add a  better logic in the Navbar component. Now take care of the current url when page is refreshed

--- dev/js/appWrapper/navbar/index.jsx
+++ dev/js/appWrapper/navbar/index.jsx
@@ -3,24 +3,57 @@ import { Link } from 'react-router-dom';
 import $ from 'jquery';
 import './navbar.scss';
 
-
 class Navbar extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUrl: window.location.pathname,
+    }
+  }
 
   componentDidMount() {
-    $("#navbar-component a").on('click', (e) => {
-      const items = document.querySelectorAll("#navbar-component a");
-      for (let i = 0; i < items.length; i++) {
-        items[i].classList.remove('selected');
-      }
-      e.currentTarget.className = 'selected';
+    let currentUrl = window.location.pathname,
+        links = document.querySelectorAll("#navbar-component a"),
+        sortedLinks = this.sortLinks(links);
+
+    $(sortedLinks).removeClass("selected").first().addClass("selected");
+
+    $("#navbar-component a").on("click", (e) => {
+      $("#navbar-component a").removeClass("selected");
+      $(e.target).addClass("selected");
     });
+  }
+
+  // Sort Link Url by their matching url segments with the current url
+  sortLinks(links){
+    return $(links).sort((a, b) => {
+        let currentUrlSegments = this.state.currentUrl.split("/").filter(segment => segment !==""),
+            aSegments = $(a).attr("href").split("/").filter(segment => segment !==""),
+            bSegments = $(b).attr("href").split("/").filter(segment => segment !==""),
+            acounter = 0,
+            bcounter = 0;
+
+        for(let i = 0; i < aSegments.length; i++){
+          if(aSegments[i] === currentUrlSegments[i]){
+            acounter ++;
+          }
+        }
+
+        for(let i = 0; i < bSegments.length; i++){
+          if(bSegments[i] === currentUrlSegments[i]){
+            bcounter ++;
+          }
+        }
+        return bcounter - acounter;
+    })
   }
 
   render() {
     return (
       <div id="navbar-component">
         <ul>
-          <li><Link to="/" className="selected"> Home page </Link></li>
+          <li><Link to="/"> Home page </Link></li>
           <li><Link to="/counter"> Counter tool </Link></li>
           <li><Link to="/movies"> Movies </Link></li>
           <li><Link to="/about">About this site </Link></li>
